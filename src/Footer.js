ddeb03d1e2380d712051dea13070374a2898cbbe
Tripti Sharma <sharmatripti526@gmail.com>
2022-05-22 17:36:51 +0530

    Add files via upload

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../App.css';
+function Footer() {
+    return (
+        <div>
+            
+   <div className="footer">
+  <p>Terms of use  |  Privacy policy </p>
+</div>
+        </div>
+    )
+}
+
+export default Footer
